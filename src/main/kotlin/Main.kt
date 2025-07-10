17eedf4b6cc8c3ea31b12a74b760c3941ef5fc56
Edu <ediago92@gmail.com>
2025-07-10 13:37:17 +0200

    Respond to multiple Ping

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,8 +7,20 @@ fun main(args: Array<String>) {
      // Since the tester restarts your program quite often, setting SO_REUSEADDR
      // ensures that we don't run into 'Address already in use' errors
      serverSocket.reuseAddress = true
-
      val socket = serverSocket.accept() // Wait for connection from client.
-     socket.outputStream.write("+PONG\r\n".toByteArray())
+     val inputStream = socket.inputStream
+
+     while(true){
+
+          val buffer = inputStream.bufferedReader().readLine()
+
+          // if no word at all is sent exit program
+          if(buffer == ""){
+               break
+          }
+
+          socket.outputStream.write("+PONG\r\n".toByteArray())
+
+     }
      println("accepted new connection")
 }

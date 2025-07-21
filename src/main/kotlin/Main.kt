eab61cf30d91dae1972ebe4058e9c67b10e87ae2
Edu <ediago92@gmail.com>
2025-07-21 16:05:50 +0200

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,15 @@
 import java.net.ServerSocket
 import kotlinx.coroutines.*
+import java.io.BufferedReader
 import java.net.Socket
+import java.util.Locale
+import java.util.Locale.getDefault
 
 fun main(args: Array<String>) {
 
     var serverSocket = ServerSocket(6379)
 
     serverSocket.reuseAddress = true
-
-
 
     while (true) {
 
@@ -25,10 +26,39 @@ fun main(args: Array<String>) {
 fun handleConnection(socket: Socket) {
 
     while (socket.isConnected) {
-        socket.inputStream.bufferedReader().readLine()
-        socket.outputStream.write("+PONG\r\n".toByteArray())
+
+        var command = parseCommand(socket.inputStream.bufferedReader())
+
+        val response = when(command[0].uppercase(getDefault())){
+            "PING" -> "+PONG\r\n".toByteArray()
+            "ECHO" -> "$${command[1].length}\r\n${command[1]}\r\n".toByteArray()
+            else -> {"-ERR unknown command '${command[0]}'\r\n".toByteArray()}
+        }
+        socket.outputStream.write(response)
+        socket.outputStream.flush()
     }
 
-    socket.close()
+}
+
+
+fun parseCommand(reader: BufferedReader): List<String>{
+
+    return when(reader.read().toChar()){
+        '*' -> parseArray(reader)
+        else -> emptyList()
+    }
 
 }
+
+fun parseArray(reader: BufferedReader): List<String>{
+
+    var parsedArray = mutableListOf<String>()
+
+    for(i in 1..reader.readLine().toInt()){
+        reader.readLine() //element length
+        parsedArray.add(reader.readLine()) //element
+    }
+
+    return parsedArray
+}
+

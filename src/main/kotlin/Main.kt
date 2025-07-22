6005ae0c7edc79e05406e61cdda7d3c30526fe1e
Edu <ediago92@gmail.com>
2025-07-22 13:13:54 +0200

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,17 @@ import java.net.ServerSocket
 import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.net.Socket
-import java.util.Locale
 import java.util.Locale.getDefault
+
+object storage {
+    val keyValueStorage = mutableMapOf<String, String>()
+}
 
 fun main(args: Array<String>) {
 
     var serverSocket = ServerSocket(6379)
+
+    //val storage = storage
 
     serverSocket.reuseAddress = true
 
@@ -32,6 +37,8 @@ fun handleConnection(socket: Socket) {
         val response = when(command[0].uppercase(getDefault())){
             "PING" -> "+PONG\r\n".toByteArray()
             "ECHO" -> "$${command[1].length}\r\n${command[1]}\r\n".toByteArray()
+            "SET" -> setResponse(command[1], command[2])
+            "GET" -> getResponse(command[1])
             else -> {"-ERR unknown command '${command[0]}'\r\n".toByteArray()}
         }
         socket.outputStream.write(response)
@@ -62,3 +69,19 @@ fun parseArray(reader: BufferedReader): List<String>{
     return parsedArray
 }
 
+fun setResponse(key: String, value: String): ByteArray{
+
+    storage.keyValueStorage[key] = value
+
+    return "+OK\r\n".toByteArray()
+}
+
+fun getResponse(key: String): ByteArray{
+
+    return if(storage.keyValueStorage[key] != null){
+        "$${storage.keyValueStorage[key]!!.length}\r\n${storage.keyValueStorage[key]!!}\r\n".toByteArray()
+    } else {
+        "$-1\r\n".toByteArray()
+    }
+
+}
